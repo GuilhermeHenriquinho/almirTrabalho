a55e33a13c4cdf5b9dfda90f11838e657f5728f5
Guilherme Henrique Moraes Caldeira <2111550031@fema.edu.br>
2022-11-23 19:36:31 -0300

    commit

---
+++ src/main/java/com/eventoapp/models/Convidado.java
@@ -0,0 +1,59 @@
+package com.eventoapp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+public class Convidado {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@NotEmpty
+	private String rg;
+
+	@NotEmpty
+	private String nome;
+	@ManyToOne
+	private Evento evento;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Evento getEvento() {
+		return evento;
+	}
+
+	public void setEvento(Evento evento) {
+		this.evento = evento;
+	}
+
+}
---
+++ src/main/java/com/eventoapp/models/Evento.java
@@ -0,0 +1,94 @@
+package com.eventoapp.models;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+public class Evento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@NotEmpty
+	private String nome;
+	@NotEmpty
+	private String local;
+	@NotEmpty
+	private String data;
+	@NotEmpty
+	private String horario;
+	@NotNull
+	private int qtdVagas;
+
+	@OneToMany(mappedBy = "evento", cascade = CascadeType.ALL)
+	private List<Convidado> convidados;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getHorario() {
+		return horario;
+	}
+
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+
+	public List<Convidado> getConvidados() {
+		return convidados;
+	}
+
+	public void setConvidados(List<Convidado> convidados) {
+		this.convidados = convidados;
+	}
+
+	public int getQtdVagas() {
+		return qtdVagas;
+	}
+
+	public void setQtdVagas(int qtdVagas) {
+		this.qtdVagas = qtdVagas;
+	}
+	
+	
+
+}
